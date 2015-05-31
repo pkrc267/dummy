1bcb6cb02890ac3e0710fc5ccf4a5b777e4ae518
kai18 <kaustubh0163@gmail.com>
2015-05-31 18:28:47 +0530

    modified:   index.php 	assets/script/

--- index.php
+++ index.php
@@ -8,65 +8,103 @@
 	<link rel="stylesheet" type="text/css" href="asses">	
 </head>
 <body>
-	<div class=" wow bounceInUp container-fluid">
-		<div class="navbar navbar-fixed-top clearfix",role="navigation">
-			 <div class="navbar-header">
-            <div class="clearfix">
-               <a class="navbar-brand" style="color:#F7C241;" href="#">BRAND NAME</a>
-             </div>
+	<nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Title</a>
         </div>
+    
+        <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav nav-justified">
-                <li class="dropdown dropdown-hover">
-                    <a href="#" class="dropdown-toggle" data toggle="dropdown">
-                      <span="dropdown-header-text">ABOUT</span>
-                    </a>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li class="txt"><a href="#">PHILOSOPHY</a></li>
-                        <li class="txt"><a href="#">OUR COMPANY</a></li>
-                        <li class="txt"><a href="team.php">TEAM</a></li>
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li><a href="#">Separated link</a></li>
                     </ul>
-                </li>
-                 <li class="dropdown dropdown-hover">
-                    <a href="#" class="dropdown-toggle" data toggle="dropdown">
-                      <span="dropdown-header-text">PRODUCTS</span="dropdown-headeer-text">
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="txt"><a href="form/form.php">PRPODUCT1</a></li>
-                        <li class="txt"><a href="#">PRODUCT2</a></li>
-                        <li class="txt"><a href="#">PRODUCT3</a></li>
-                    </ul>
-                </li>
-                 <li class="dropdown dropdown-hover">
-                    <a href="#" class="dropdown-toggle" data toggle="dropdown">
-                      <span="dropdown-header-text">PARTNERS</span="dropdown-headeer-text">
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="txt"><a href="#">PARTNER</a></li>
-                        <li class="txt"><a href="#">PARTNER2</a></li>
-                        <li class="txt"><a href="#">PARTNER3</a></li>
-                    </ul>
-                </li>
-                 <li class="dropdown dropdown-hover">
-                    <a href="#" class="dropdown-toggle" data toggle="dropdown">
-                      <span="dropdown-header-text">REGISTER</span="dropdown-headeer-text">
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="txt"><a href="index.php?id=form">USER</a></li>
-                        <li class="txt"><a href="#"></a><h9>(Coming Soon)</h9> </li>
-                         <li class="txt"><a href="#">PRESS CORP</a><h9>(Coming Soon)</h9></li>
-                    </ul>
-                </li>
-                 <li class="dropdown dropdown-hover">
-                    <a href="form/contact.php" class="dropdown-toggle" data toggle="dropdown">
-                      <span="dropdown-header-text">CONTACT</span="dropdown-header-text">
-                    </a>
                 </li>
             </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+    <div id="carousel-id" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-id" data-slide-to="0" class=""></li>
+            <li data-target="#carousel-id" data-slide-to="1" class=""></li>
+            <li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="item">
+                <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojN2E3YTdhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+Rmlyc3Qgc2xpZGU8L3RleHQ+PC9zdmc+">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Example headline.</h1>
+                        <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNmE2YTZhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+U2Vjb25kIHNsaWRlPC90ZXh0Pjwvc3ZnPg==">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Another example headline.</h1>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="item active">
+                <img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzU1NSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNWE1YTVhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+VGhpcmQgc2xpZGU8L3RleHQ+PC9zdmc+">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>One more for good measure.</h1>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
+        <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+        <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
     </div>
-		</div>
-	</div>
+    <div class="row">
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>    
+    </div>
+    <!-- Latest compiled and minified CSS & JS -->
+    <script src="//code.jquery.com/jquery.js"></script>
 	<script src="assets/css/bootstrap/js/bootstrap.min.js"></script>
    		<script>
 	   		jQuery('ul.nav li.dropdown-hover').hover(function() 
@@ -88,5 +126,11 @@
  		new WOW().init();
 	</script>
 </body>
-
+<footer>
+    <div class="panel panel-default">
+        <div class="panel-body">
+           Basic panel example
+        </div>
+    </div>
+</footer>
 </html>
